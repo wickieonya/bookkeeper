ba09ee9d698de01c8298fe2262f8439c1c8258cb
wickieonya <wickieonya@gmail.com>
2022-05-04 14:20:59 +0300

    programmatic routing

--- src/routes/invoice.js
+++ src/routes/invoice.js
@@ -1,7 +1,13 @@
-import {useParams} from "react-router-dom";
-import {getInvoice} from "../data";
+import {
+    useParams,
+    useNavigate,
+    useLocation,
+} from "react-router-dom";
+import {getInvoice, deleteInvoice} from "../data";
 
 export default function Invoice() {
+    let navigate = useNavigate();
+    let location = useLocation();
     let params = useParams();
     let invoice = getInvoice(parseInt(params.invoiceId, 10));
     return (
@@ -11,6 +17,16 @@ export default function Invoice() {
                 {invoice.name}: {invoice.number}
             </p>
             <p>Due date: {invoice.due}</p>
+            <p>
+                <button
+                    onClick={() => {
+                        deleteInvoice(invoice.number);
+                        navigate('/invoices' + location.search);
+                    }}
+                >
+                    Delete
+                </button>
+            </p>
         </main>
     );
 }
