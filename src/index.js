f20f96f2aedb49ae218ea522ba84d4fefaac4050
wickieonya <wickieonya@gmail.com>
2022-05-04 12:14:43 +0300

    updated invoice

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {
 import App from "./App";
 import Expenses from "./routes/expenses";
 import Invoices from "./routes/invoices";
+import Invoice from "./routes/invoice";
 
 const rootElement = document.getElementById("root");
 render(
@@ -14,7 +15,9 @@ render(
         <Routes>
             <Route path="/" element={<App/>}>
                 <Route path="expenses" element={<Expenses/>}/>
-                <Route path="invoices" element={<Invoices/>}/>
+                <Route path="invoices" element={<Invoices/>}>
+                    <Route path=":invoiceId" element={<Invoice/>}/>
+                </Route>
                 <Route
                     path="*"
                     element={
